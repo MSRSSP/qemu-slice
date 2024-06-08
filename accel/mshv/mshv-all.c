06860114a67b76e77987a218968a1c14988528c1
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2024-06-07 21:42:38 -0700

    format file using qemu clang-format locally && fix bugs

--- accel/mshv/mshv-all.c
+++ accel/mshv/mshv-all.c
@@ -7,212 +7,225 @@
 #include "qemu/error-report.h"
 #include "sysemu/cpus.h"
 
+#include "qemu/log.h"
+
 #include "hw/boards.h"
+#include "sysemu/runstate.h" //vm_stop
 #include "sysemu/stats.h"
 
+#include "hw/i386/x86.h" //ioapic_eoi_broadcast
+#include "qemu/guest-random.h"
 #include <mshv.h>
 
-typedef struct MSHVState {
-	AccelState parent_obj;
-	struct MshvHypervisor mshv;
-	struct MshvVM vm;
-} MSHVState;
+typedef struct MshvState {
+    AccelState parent_obj;
+    MshvHypervisorC *mshv;
+    MshvVmC *vm;
+} MshvState;
 
 #define TYPE_MSHV_ACCEL ACCEL_CLASS_NAME("mshv")
 
-DECLARE_INSTANCE_CHECKER(MSHVState, MSHV_STATE, TYPE_MSHV_ACCEL)
+DECLARE_INSTANCE_CHECKER(MshvState, MSHV_STATE, TYPE_MSHV_ACCEL)
 
 bool mshv_allowed;
 
-MSHVState *mshv_state;
+MshvState *mshv_state;
 
 static int mshv_init(MachineState *ms)
 {
-	MachineClass *mc = MACHINE_GET_CLASS(ms);
-	MSHVState *s;
-	VmType type;
+    MachineClass *mc = MACHINE_GET_CLASS(ms);
+    MshvState *s;
+    uint64_t vm_type;
+    s = MSHV_STATE(ms->accelerator);
 
-	s = MSHV_STATE(ms->accelerator);
+    s->mshv = mshv_new();
+    s->vm = NULL;
 
-	s->mshv = mshv_new();
-	s->vm = NULL
+    // TODO: object_property_find(OBJECT(current_machine), "mshv-type")
+    vm_type = 0;
+    do {
+        qemu_log_mask(LOG_GUEST_ERROR, "%s: mshv_create_vm_with_type\n",
+                      __func__);
+        s->vm = mshv_create_vm_with_type(s->mshv, vm_type);
+    } while (s->vm == NULL);
 
-		// TODO: object_property_find(OBJECT(current_machine), "mshv-type")
-		type = Normal;
-	do {
-		qemu_log_mask(LOG_GUEST_ERROR, "%s: mshv_create_vm_with_type\n",
-			      __func__);
-		s->vm = mshv_create_vm_with_type(s->mshv, type);
-	} while (s->vm == NULL);
+    mc->default_ram_id = NULL;
+    mshv_state = s;
 
-	mc->default_ram_id = NULL;
-	mshv_state = s;
-
-	return 0;
+    return 0;
 }
 
 static void mshv_accel_class_init(ObjectClass *oc, void *data)
 {
-	AccelClass *ac = ACCEL_CLASS(oc);
+    AccelClass *ac = ACCEL_CLASS(oc);
 
-	ac->name = "MSHV";
-	ac->init_machine = mshv_init;
-	ac->allowed = &mshv_allowed;
+    ac->name = "MSHV";
+    ac->init_machine = mshv_init;
+    ac->allowed = &mshv_allowed;
 }
 
 static void mshv_accel_instance_init(Object *obj)
 {
-	MSHVState *s = MSHV_STATE(obj);
+    MshvState *s = MSHV_STATE(obj);
 
-	s->mshv.mshv = NULL;
-	s->vm.vm = NULL;
+    s->mshv = NULL;
+    s->vm = NULL;
 }
 
 static const TypeInfo mshv_accel_type = {
-	.name = TYPE_MSHV_ACCEL,
-	.parent = TYPE_ACCEL,
-	.instance_init = mshv_accel_instance_init,
-	.class_init = mshv_accel_class_init,
-	.instance_size = sizeof(MSHVState),
+    .name = TYPE_MSHV_ACCEL,
+    .parent = TYPE_ACCEL,
+    .instance_init = mshv_accel_instance_init,
+    .class_init = mshv_accel_class_init,
+    .instance_size = sizeof(MshvState),
 };
 
 static int mshv_init_vcpu(CPUState *cpu)
 {
-	int r;
-
-	cpu->opaque = (void *)mshv_new_vcpu(mshv_state->vm, cpu->cpu_index);
-	return 0;
+    cpu->opaque = (void *)mshv_new_vcpu(mshv_state->vm, cpu->cpu_index);
+    return 0;
 }
 
-static struct MshvVCpu *mshv_vcpu(CPUState *cpu)
+static int mshv_destroy_vcpu(CPUState *cpu)
 {
-	(struct MshvVCpu *)cpu->opaque
+    cpu->opaque = NULL;
+    return 0;
 }
 
-void mshv_run_vcpu_qemu(CPUState *cpu)
+inline static MshvVcpuC *mshv_vcpu(CPUState *cpu)
 {
-	MshvVCpu *mshv_cpu = mshv_vcpu(cpu);
-	u8 vector;
+    return (MshvVcpuC *)cpu->opaque;
+}
 
-	bql_unlock();
-	cpu_exec_start(cpu);
+int mshv_run_vcpu_qemu(CPUState *cpu);
 
-	do {
-		MemTxAttrs attrs;
+int mshv_run_vcpu_qemu(CPUState *cpu)
+{
+    MshvVcpuC *mshv_cpu = mshv_vcpu(cpu);
+    uint8_t vector;
+    int ret = 0;
 
-		if (cpu->vcpu_dirty) {
-			//ret = kvm_arch_put_registers(cpu, KVM_PUT_RUNTIME_STATE);
-			//if (ret) {
-			error_report("Failed to put registers after init: %s",
-				     strerror(-ret));
-			//    ret = -1;
-			break;
-			//}
+    bql_unlock();
+    cpu_exec_start(cpu);
 
-			cpu->vcpu_dirty = false;
-		}
+    do {
+        if (cpu->vcpu_dirty) {
+            // ret = kvm_arch_put_registers(cpu, KVM_PUT_RUNTIME_STATE);
+            // if (ret) {
+            error_report("Failed to put registers after init: %s",
+                         strerror(-ret));
+            ret = -1;
+            break;
+            //}
 
-		//kvm_arch_pre_run(cpu, run);
-		if (qatomic_read(&cpu->exit_request)) {
-			/*
+            cpu->vcpu_dirty = false;
+        }
+
+        // kvm_arch_pre_run(cpu, run);
+        if (qatomic_read(&cpu->exit_request)) {
+            /*
              * KVM requires us to reenter the kernel after IO exits to complete
              * instruction emulation. This self-signal will ensure that we
              * leave ASAP again.
              */
-			//kvm_cpu_kick_self();
-		}
+            // kvm_cpu_kick_self();
+        }
 
-		/* Read cpu->exit_request before KVM_RUN reads run->immediate_exit.
+        /* Read cpu->exit_request before KVM_RUN reads run->immediate_exit.
          * Matching barrier in kvm_eat_signals.
          */
-		smp_rmb();
+        smp_rmb();
 
-		MshvVmExit exit = mshv_run_vcpu(mshv_cpu, &vector);
-		if (exit == IoapicEoi) {
-			ioapic_eoi_broadcast(vector);
-		} else if (exit == Ignore) {
-		} else if (exit == Reset) {
-			qemu_system_reset_request(SHUTDOWN_CAUSE_GUEST_RESET);
-		} else if (exit == Shutdown) {
-			qemu_system_reset_request(SHUTDOWN_CAUSE_GUEST_RESET);
-		} else {
-			break;
-		}
+        MshvVmExit exit = mshv_run_vcpu(mshv_cpu, &vector);
+        switch (exit) {
+        case IoapicEoi:
+            ioapic_eoi_broadcast(vector);
+            break;
+        case Reset:
+        case Shutdown:
+            qemu_system_reset_request(SHUTDOWN_CAUSE_GUEST_RESET);
+            ret = EXCP_INTERRUPT;
+            break;
+        case Ignore:
+            break;
+        default:
+            break;
+        }
+    } while (ret == 0);
+
+    cpu_exec_end(cpu);
+    bql_lock();
+
+    if (ret < 0) {
+        cpu_dump_state(cpu, stderr, CPU_DUMP_CODE);
+        vm_stop(RUN_STATE_INTERNAL_ERROR);
     }
 
-	cpu_exec_end(cpu);
-	bql_lock();
-
-	if (ret < 0) {
-		cpu_dump_state(cpu, stderr, CPU_DUMP_CODE);
-		vm_stop(RUN_STATE_INTERNAL_ERROR);
-	}
-
-	qatomic_set(&cpu->exit_request, 0);
-	return ret;
+    qatomic_set(&cpu->exit_request, 0);
+    return ret;
 }
 static void *mshv_vcpu_thread_fn(void *arg)
 {
-	CPUState *cpu = arg;
-	int r;
+    CPUState *cpu = arg;
 
-	rcu_register_thread();
+    rcu_register_thread();
 
-	bql_lock();
-	qemu_thread_get_self(cpu->thread);
-	cpu->thread_id = qemu_get_thread_id();
-	current_cpu = cpu;
+    bql_lock();
+    qemu_thread_get_self(cpu->thread);
+    cpu->thread_id = qemu_get_thread_id();
+    current_cpu = cpu;
+    mshv_init_vcpu(cpu);
 
-	/* signal CPU creation */
-	cpu_thread_signal_created(cpu);
-	qemu_guest_random_seed_thread_part2(cpu->random_seed);
+    /* signal CPU creation */
+    cpu_thread_signal_created(cpu);
+    qemu_guest_random_seed_thread_part2(cpu->random_seed);
 
-	do {
-		if (cpu_can_run(cpu)) {
-			mshv_run_vcpu_qemu(cpu);
-		}
-		qemu_wait_io_event(cpu);
-	} while (!cpu->unplug || cpu_can_run(cpu));
+    do {
+        if (cpu_can_run(cpu)) {
+            mshv_run_vcpu_qemu(cpu);
+        }
+        qemu_wait_io_event(cpu);
+    } while (!cpu->unplug || cpu_can_run(cpu));
 
-	kvm_destroy_vcpu(cpu);
-	cpu_thread_signal_destroyed(cpu);
-	bql_unlock();
-	rcu_unregister_thread();
-	return NULL;
+    mshv_destroy_vcpu(cpu);
+    cpu_thread_signal_destroyed(cpu);
+    bql_unlock();
+    rcu_unregister_thread();
+    return NULL;
 }
 
 static void mshv_start_vcpu_thread(CPUState *cpu)
 {
-	char thread_name[VCPU_THREAD_NAME_SIZE];
+    char thread_name[VCPU_THREAD_NAME_SIZE];
 
-	cpu->thread = g_malloc0(sizeof(QemuThread));
-	cpu->halt_cond = g_malloc0(sizeof(QemuCond));
-	qemu_cond_init(cpu->halt_cond);
+    cpu->thread = g_malloc0(sizeof(QemuThread));
+    cpu->halt_cond = g_malloc0(sizeof(QemuCond));
+    qemu_cond_init(cpu->halt_cond);
 
-	qemu_log_mask(LOG_GUEST_ERROR, "%s: thread_name = %s, cpu = %d\n",
-		      __func__, thread_name, cpu->cpu_index);
-	qemu_thread_create(cpu->thread, thread_name, mshv_vcpu_thread_fn, cpu,
-			   QEMU_THREAD_JOINABLE);
+    qemu_log_mask(LOG_GUEST_ERROR, "%s: thread_name = %s, cpu = %d\n", __func__,
+                  thread_name, cpu->cpu_index);
+    qemu_thread_create(cpu->thread, thread_name, mshv_vcpu_thread_fn, cpu,
+                       QEMU_THREAD_JOINABLE);
 }
 
 static void mshv_accel_ops_class_init(ObjectClass *oc, void *data)
 {
-	AccelOpsClass *ops = ACCEL_OPS_CLASS(oc);
+    AccelOpsClass *ops = ACCEL_OPS_CLASS(oc);
 
-	ops->create_vcpu_thread = mshv_start_vcpu_thread;
+    ops->create_vcpu_thread = mshv_start_vcpu_thread;
 }
 
 static const TypeInfo mshv_accel_ops_type = {
-	.name = ACCEL_OPS_NAME("mshv"),
-	.parent = TYPE_ACCEL_OPS,
-	.class_init = mshv_accel_ops_class_init,
-	.abstract = true,
+    .name = ACCEL_OPS_NAME("mshv"),
+    .parent = TYPE_ACCEL_OPS,
+    .class_init = mshv_accel_ops_class_init,
+    .abstract = true,
 };
 
 static void mshv_type_init(void)
 {
-	type_register_static(&mshv_accel_type);
-	type_register_static(&mshv_accel_ops_type);
+    type_register_static(&mshv_accel_type);
+    type_register_static(&mshv_accel_ops_type);
 }
 
 type_init(mshv_type_init);
